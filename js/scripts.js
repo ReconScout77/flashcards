569ce15dc07814a2db99b18682e35ef4ad1dcc97
Kaili Nishihira and Robert Murray <kaili@>
2017-07-05 09:29:17 -0700

    Add span classes to all div sets.

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+  $(".click-js").click(function() {
+    $("#javascript-term").toggle();
+    $("#javascript-definition").toggle();
+  });
+
+  $(".click-operators").click(function() {
+    $("#operators-term").toggle();
+    $("#operators-definition").toggle();
+  });
+
+  $(".click-variables").click(function() {
+    $("#variables-term").toggle();
+    $("#variables-definition").toggle();
+  });
+
+  $(".click-variablename").click(function() {
+    $("#variablename-term").toggle();
+    $("#variablename-definition").toggle();
+  });
+
+  $(".click-functions").click(function() {
+    $("#functions-term").toggle();
+    $("#functions-definition").toggle();
+  });
+
+  $(".click-methods").click(function() {
+    $("#methods-term").toggle();
+    $("#methods-definition").toggle();
+  });
+
+  $(".click-arguments").click(function() {
+    $("#arguments-term").toggle();
+    $("#arguments-definition").toggle();
+  });
+
+  $(".click-parameters").click(function() {
+    $("#parameters-term").toggle();
+    $("#parameters-definition").toggle();
+  });
+
+  $(".click-return").click(function() {
+    $("#return-term").toggle();
+    $("#return-definition").toggle();
+  });
+
+  $(".click-chainingmethods").click(function() {
+    $("#chainingmethods-term").toggle();
+    $("#chainingmethods-definition").toggle();
+  });
+
+  $(".click-strings").click(function() {
+    $("#strings-term").toggle();
+    $("#strings-definition").toggle();
+  });
+
+  $(".click-booleans").click(function() {
+    $("#booleans-term").toggle();
+    $("#booleans-definition").toggle();
+  });
+
+  $(".click-undefined").click(function() {
+    $("#undefined-term").toggle();
+    $("#undefined-definition").toggle();
+  });
+
+  $(".click-nan").click(function() {
+    $("#nan-term").toggle();
+    $("#nan-definition").toggle();
+  });
+
+  $(".click-escape").click(function() {
+    $("#escape-term").toggle();
+    $("#escape-definition").toggle();
+  });
+
+  $(".click-alert").click(function() {
+    $("#alert-term").toggle();
+    $("#alert-definition").toggle();
+  });
+
+  $(".click-comments").click(function() {
+    $("#comments-term").toggle();
+    $("#comments-definition").toggle();
+  });
+
+  $(".click-jquery").click(function() {
+    $("#jquery-term").toggle();
+    $("#jquery-definition").toggle();
+  });
+
+  $(".click-attributes").click(function() {
+    $("#attributes-term").toggle();
+    $("#attributes-definition").toggle();
+  });
+
+});
